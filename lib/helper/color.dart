4f88518be10be72a0da791902fcf85f17e278a92
Abdalaziz abdallah <abdalazizabdalluh3@gmail.com>
2022-07-05 16:20:20 +0300

    update widget

---
+++ lib/helper/color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class ColorsHelper {
+  static Color myPuprle = const Color(0xFF7646FF);
+  static Color myHighPriority = const Color(0xFFFC5565);
+  static Color myBlack = const Color(0xFF172735);
+  static Color myGreen = const Color(0xFF4CD964);
+  static Color myGray = const Color(0xFFF6F6F8);
+}
--- lib/widgets/to_do_widget.dart
+++ lib/widgets/to_do_widget.dart
@@ -1,6 +1,10 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:to_do_app/helper/color.dart';
 
 class MyToDoWidget extends StatefulWidget {
   const MyToDoWidget({Key? key}) : super(key: key);
@@ -12,51 +16,88 @@ class MyToDoWidget extends StatefulWidget {
 class _MyToDoWidgetState extends State<MyToDoWidget> {
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      tileColor: Colors.white,
-      trailing: Container(
-        height: double.infinity ,
-        width: 100,
-        color: Colors.amberAccent,
-        child: Row(
-          children: <Widget>[
-            IconButton(
-                onPressed: () {}, icon: const Icon(Icons.star_border_rounded,size: 30,)),
-            Container(
-              height: 25,
-              decoration: const BoxDecoration(
-                  color: Colors.white,
-                  shape: BoxShape.circle,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black12,
-                      offset: Offset.zero,
-                      blurRadius: 5,
-                      spreadRadius: 0,
-                    )
-                  ]),
-              child:
-                  const CircleAvatar(radius: 20, backgroundColor: Color(0xFFFC5565)),
-            ),
-          ],
+    TextTheme _textTheme = Theme.of(context).textTheme;
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(width: 16.0, color: ColorsHelper.myGray),
         ),
       ),
-      title: const Text('Metting with client'),
-      subtitle: const Text('Official'),
-      leading: SizedBox(
-        width: 50,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            const Text('10:00'),
-            RichText(
-                text: const TextSpan(
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20,color: Colors.black),
-                    children: [
-                  TextSpan(text: 'AM'),
-                ]))
-          ],
-        ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Container(
+            color: ColorsHelper.myGreen,
+            width: 216.w,
+            height: 48.w,
+            child: Center(
+                child: Text(
+              'Today',
+              style: _textTheme.subtitle2?.copyWith(color: Colors.white),
+            )),
+          ),
+          ListTile(
+            contentPadding: EdgeInsets.symmetric(horizontal: 50.w),
+            minLeadingWidth: 50.w,
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                IconButton(
+                    padding: EdgeInsets.zero,
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.star_border_rounded,
+                    )),
+                Container(
+                  height: 48.w,
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black26,
+                          offset: Offset.zero,
+                          blurRadius: 5,
+                          spreadRadius: 0,
+                        )
+                      ]),
+                  child: CircleAvatar(
+                    radius: 48.w,
+                    backgroundColor: ColorsHelper.myHighPriority,
+                  ),
+                ),
+              ],
+            ),
+            title: Text(
+              'Metting with client',
+              style: _textTheme.headline3,
+            ),
+            subtitle: Text(
+              'Official',
+              style: _textTheme.subtitle1,
+            ),
+            leading: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  '10:00',
+                  style: _textTheme.headline3,
+                ),
+                RichText(
+                  text: TextSpan(
+                    style: _textTheme.headline3,
+                    children: const [
+                      TextSpan(
+                        text: 'AM',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
